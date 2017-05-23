d35b602dd622e64afac1777ed40867aed77076c9
Yash Pahade <yash.aries.6@gmail.com>
2017-05-23 15:32:59 -0700

    first commit

---
+++ newsReader/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  newsReader
+//
+//  Created by Yash Pahade on 5/22/17.
+//  Copyright © 2017 Yash Pahade. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+
+}
---
+++ newsReader/articleCell.swift
@@ -0,0 +1,24 @@
+//
+//  articleCell.swift
+//  newsReader
+//
+//  Created by Yash Pahade on 5/22/17.
+//  Copyright © 2017 Yash Pahade. All rights reserved.
+//
+
+import UIKit
+
+class articleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
